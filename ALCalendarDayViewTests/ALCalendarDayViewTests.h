c137cf00ba00aa6b2ca061a2740b5fa44b8d6d41
Sergei Zaplitny <zaplitny@gmail.com>
2012-08-07 14:40:04 +0300

    Empty project initial commit

---
+++ ALCalendarDayViewTests/ALCalendarDayViewTests.h
@@ -0,0 +1,5 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ALCalendarDayViewTests : SenTestCase
+
+@end
---
+++ ALCalendarDayViewTests/ALCalendarDayViewTests.m
@@ -0,0 +1,24 @@
+#import "ALCalendarDayViewTests.h"
+
+@implementation ALCalendarDayViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ALCalendarDayViewTests");
+}
+
+@end
