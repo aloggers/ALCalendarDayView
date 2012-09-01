368c8ef328ec7cd1b3e7f9d486b876703f3577db
Sergei Zaplitny <zaplitny@gmail.com>
2012-09-01 19:50:38 +0300

    Added title/description

--- ALCalendarDayView/ALCalendarTileView.m
+++ ALCalendarDayView/ALCalendarTileView.m
@@ -1,21 +1,52 @@
 #import "ALCalendarTileView.h"
 #import "ALCalendarEvent.h"
 
+@interface ALCalendarTileView()
+@property (nonatomic, strong) UILabel* titleLabel;
+@property (nonatomic, strong) UILabel* descriptionLabel;
+@end
 
 @implementation ALCalendarTileView {
-
 @private
     ALCalendarEvent* _event;
+    UILabel* _titleLabel;
+    UILabel* _descriptionLabel;
 }
 @synthesize event = _event;
+@synthesize titleLabel = _titleLabel;
+@synthesize descriptionLabel = _descriptionLabel;
+
 
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
+        _titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        _titleLabel.backgroundColor = [UIColor clearColor];
+        _titleLabel.textColor = [UIColor whiteColor];
+        _titleLabel.shadowColor = [UIColor colorWithWhite:0.0f alpha:0.5f];
+        _titleLabel.font = [UIFont boldSystemFontOfSize:14.0f];
+        [self addSubview:_titleLabel];
 
+        _descriptionLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        _descriptionLabel.backgroundColor = [UIColor clearColor];
+        _descriptionLabel.textColor = [UIColor whiteColor];
+        _descriptionLabel.shadowColor = [UIColor colorWithWhite:0.0f alpha:0.5f];
+        _descriptionLabel.font = [UIFont systemFontOfSize:12.0f];
+        _descriptionLabel.lineBreakMode = UILineBreakModeWordWrap;
+        _descriptionLabel.numberOfLines = 0;
+        [self addSubview:_descriptionLabel];
+
+        self.backgroundColor = [UIColor darkGrayColor];
     }
 
     return self;
+}
+
+- (void)setEvent:(ALCalendarEvent*)event {
+    _event = event;
+    _titleLabel.text = event.title;
+    _descriptionLabel.text = event.description;
+    self.backgroundColor = event.color;
 }
 
 - (void)layoutSubviews {
@@ -25,6 +56,9 @@
     else {
         self.backgroundColor = [UIColor darkGrayColor];
     }
+    _titleLabel.frame = CGRectMake(6, 3, self.frame.size.width - 12, 16);
+    _descriptionLabel.frame = CGRectMake(6, _titleLabel.frame.size.height + 2,
+            self.frame.size.width - 12, self.frame.size.height - _titleLabel.frame.size.height - 14);
 }
 
 @end
--- ALCalendarDayView/DemoViewController.m
+++ ALCalendarDayView/DemoViewController.m
@@ -20,16 +20,30 @@
     NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm"];
     NSMutableArray* result = [[NSMutableArray alloc] init];
+
+    ALCalendarEvent* sleepEvent = [[ALCalendarEvent alloc] init];
+    sleepEvent.start = [dateFormatter dateFromString:@"2012-08-08 03:00"];
+    sleepEvent.end = [dateFormatter dateFromString:@"2012-08-08 08:00"];
+    sleepEvent.color = [UIColor colorWithRed:0.1 green:0.9 blue:0.1 alpha:0.6];
+    sleepEvent.title = @"Sleep";
+    [result addObject:sleepEvent];
+
     ALCalendarEvent* workEvent = [[ALCalendarEvent alloc] init];
     workEvent.start = [dateFormatter dateFromString:@"2012-08-08 09:00"];
     workEvent.end = [dateFormatter dateFromString:@"2012-08-08 19:00"];
-    workEvent.color = [UIColor colorWithRed:0.1 green:0.2 blue:0.9 alpha:0.4];
+    workEvent.color = [UIColor colorWithRed:0.1 green:0.2 blue:0.9 alpha:0.6];
+    workEvent.title = @"Work";
+    workEvent.description = @"Developing day calendar view component. Discussing aTimeLogger UI with designer";
+    [result addObject:workEvent];
+
     ALCalendarEvent* dinnerEvent = [[ALCalendarEvent alloc] init];
     dinnerEvent.start = [dateFormatter dateFromString:@"2012-08-08 13:00"];
     dinnerEvent.end = [dateFormatter dateFromString:@"2012-08-08 14:00"];
-    dinnerEvent.color = [UIColor colorWithRed:0 green:0.9 blue:0.1 alpha:0.4];
+    dinnerEvent.color = [UIColor colorWithRed:0 green:0.9 blue:0.1 alpha:0.6];
+    dinnerEvent.title = @"Dinner";
+    dinnerEvent.description = @"Some sea food";
     [result addObject:dinnerEvent];
-    [result addObject:workEvent];
+
     return result;
 }
 
